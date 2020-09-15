39a94420ffbd0cf305a43cecdb40d4112fd51ab2
yjx <578755236@qq.com>
2020-09-15 20:58:44 +0800

    个人信息页面

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,21 +1,131 @@
 <template>
   <div class="layout">
-    <section class="page-header" :style="'background-image: linear-gradient(120deg, '+backgroundColorLeft+', '+backgroundColorRight+');color: '+fontColor+';'">
+    <section
+      class="page-header"
+      :style="'background-image: linear-gradient(120deg, '+backgroundColorLeft+', '+backgroundColorRight+');color: '+fontColor+';'"
+    >
       <div style="position:absolute; top:20px; right:20px; z-index:2;">
-          <el-tooltip effect="dark" :content="fullButton.full?'退出':'全屏'" placement="bottom-end">
-              <el-button @click="full" :icon="fullButton.full?'el-icon-close':'el-icon-rank'" circle></el-button>
-          </el-tooltip>
+        <el-tooltip effect="dark" :content="fullButton.full?'退出':'全屏'" placement="bottom-end">
+          <el-button @click="full" :icon="fullButton.full?'el-icon-close':'el-icon-rank'" circle></el-button>
+        </el-tooltip>
       </div>
-      <div v-for="(item,index) in randomIcon" :key="'ri'+index" :style="'position:absolute; top:'+item.top+'px; left:'+item.left+'px; z-index:1;'">
-          <font :style="'font-size: '+item.size+'px;color:#fff;'">
-              <b>♪</b>
-          </font>
+      <div
+        v-for="(item,index) in randomIcon"
+        :key="'ri'+index"
+        :style="'position:absolute; top:'+item.top+'px; left:'+item.left+'px; z-index:1;'"
+      >
+        <font :style="'font-size: '+item.size+'px;color:#fff;'">
+          <b>♪</b>
+        </font>
       </div>
       <h1 class="project-name">{{blogTitle}}</h1>
       <h2 class="project-tagline">{{blogDescribe}}</h2>
       <a :href="'https://github.com/'+githubUsername" class="btn" target="_blank">GitHub主页</a>
       <a href="https://github.com/GitHub-Laziji/vblog" class="btn" target="_blank" v-if="!mini">博客源码</a>
     </section>
+    <!-- 个人信息 -->
+    <div style="position:relative;  z-index:2;margin: auto;margin-top:-30px;width:64rem;">
+      <el-card shadow="never" :body-style="{ padding: '0px' }">
+        <el-row>
+          <el-col :span="10">
+            <el-menu
+              @select="selectTopbar"
+              :default-active="topbar.active"
+              mode="horizontal"
+              menu-trigger="click"
+            >
+              <el-submenu index="#more">
+                <template slot="title">了解博主</template>
+                <el-menu-item index="#githubHome">github主页</el-menu-item>
+                <el-menu-item index="#blog">其他博客</el-menu-item>
+              </el-submenu>
+              <el-submenu index="#webSites" v-if="webSites.length>0">
+                <template slot="title">其他网站</template>
+                <el-menu-item
+                  :index="'#webSites-'+index"
+                  v-for="(item,index) in webSites"
+                  :key="'#webSites'+index"
+                >{{item.name}}</el-menu-item>
+              </el-submenu>
+            </el-menu>
+          </el-col>
+          <el-col :span="8" style="text-align: center;padding: 12px 0px 0px 10px">
+            <el-row>
+              <el-col :span="4">
+                <el-popover placement="top" trigger="hover">
+                  <div style="text-align: center">
+                    <el-progress color="#67C23A" type="circle" :percentage="music.volume"></el-progress>
+                    <br />
+                    <el-button @click="changeVolume(-10)" icon="el-icon-minus" circle></el-button>
+                    <el-button @click="changeVolume(10)" icon="el-icon-plus" circle></el-button>
+                  </div>
+
+                  <el-button
+                    @click="play"
+                    id="play"
+                    slot="reference"
+                    :icon="music.isPlay?'el-icon-refresh':'el-icon-caret-right'"
+                    circle
+                  ></el-button>
+                </el-popover>
+              </el-col>=
+              <el-col :span="14" style="padding-left: 20px">
+                <el-slider
+                  @change="changeTime"
+                  :format-tooltip="formatTime"
+                  :max="music.maxTime"
+                  v-model="music.currentTime"
+                  style="width: 100%;"
+                ></el-slider>
+              </el-col>
+              <el-col
+                :span="6"
+                style="padding: 9px 0px 0px 10px;color:#909399;font-size: 13px"
+              >{{formatTime(music.currentTime)}}/{{formatTime(music.maxTime)}}</el-col>
+            </el-row>
+
+            <audio ref="music" loop autoplay v-if="audioAutoPlay">
+              <source :src="audioUrl" type="audio/mpeg" />
+            </audio>
+            <audio ref="music" loop v-else>
+              <source :src="audioUrl" type="audio/mpeg" />
+            </audio>
+          </el-col>
+          <el-col :span="4" style="text-align: right;">
+            <div style="font-size: 20px;color:#606266;margin-top: 5px">
+              <b>{{githubUsername}}</b>
+            </div>
+            <div style="color:#606266;">
+              <i class="el-icon-location"></i>
+              &nbsp;{{location ? location: '未填写地址'}}
+              <br />
+            </div>
+          </el-col>
+          <el-col :span="2" style="text-align: center;">
+            <img
+              v-popover:bigAvatar
+              :src="avatarUrl"
+              style="margin-top: 4px;margin-right: 10px;width:52px; height:52px; border-radius:5px; border: 1px solid #EBEEF5"
+            />
+            <el-popover
+              ref="bigAvatar"
+              placement="top-start"
+              :title="githubUsername"
+              width="200"
+              trigger="hover"
+            >
+              <i class="el-icon-star-on"></i>
+              &emsp;{{name}}
+              <br />
+              <i class="el-icon-location"></i>
+              &emsp;{{location}}
+              <br />
+              <img :src="avatarUrl" style="width: 200px;height: 200px;" />
+            </el-popover>
+          </el-col>
+        </el-row>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -27,154 +137,223 @@ query {
 }
 </static-query>
 <script>
-  export default {
-    data() {
-      return {
-        music: {
-          isPlay: false,
-          currentTime: 0,
-          maxTime: 0,
-          volume: 100
-        },
-        backgroundColorLeft: '#155799',
-        fontColor: '#ffffff',
-        backgroundColorRight: '#FC2D2D',
-        blogTitle: 'Laziji',
-        blogDescribe: '欢迎来到杨家馨的个人博客。',
-        githubUsername: 'GitHub-yangjiaxin',
-        fullButton: {
-            full: false
-        },
-        topbar: {
-            active: "",
-        },
-        randomIcon: []
+export default {
+  data() {
+    return {
+      music: {
+        isPlay: false,
+        currentTime: 0,
+        maxTime: 0,
+        volume: 100,
+      },
+      mini: true,
+      backgroundColorLeft: "#2690F9",
+      fontColor: "#ffffff",
+      backgroundColorRight: "#FC2D2D",
+      blogTitle: "Laziji",
+      blogDescribe: "欢迎来到杨家馨的个人博客。",
+      githubUsername: "yangjiaxin",
+      fullButton: {
+        full: false,
+      },
+      topbar: {
+        active: "",
+      },
+      location: '',
+      avatarUrl: 'https://portrait.gitee.com/uploads/avatars/user/268/805129_dadami_1592040803.png!avatar200',
+      randomIcon: [],
+      name: '大白菜',
+      webSites:[
+        {"name":"v2ex" , "url":"https://www.v2ex.com/member/laziji"}
+      ],
+      audioAutoPlay: false,
+      audioUrl: "http://sc1.111ttt.cn:8282/2018/1/03m/13/396131232171.m4a?tflag=1519095601&pin=6cd414115fdb9a950d827487b16b5f97#.mp3",
+    };
+  },
+  methods: {
+    selectTopbar(index) {
+      //取消菜单选中状态
+      this.topbar.active = this.topbar.active == "" ? " " : "";
+      switch (index) {
+        case "#githubHome":
+          window.open("https://github.com/" + this.githubUsername);
+          break;
+        case "#blog":
+          if (this.blog) {
+            window.open(
+              (this.blog.match(/https?:\/\//i) ? "" : "https://") + this.blog
+            );
+          } else {
+            this.$message({
+              message: "博主没有其他博客",
+              type: "info",
+            });
+          }
+          break;
+        default:
+          if (/#webSites-\d+/.test(index)) {
+            let i = parseInt(index.split("-")[1]);
+            let url = this.webSites[i].url;
+            window.open((url.match(/https?:\/\//i) ? "" : "https://") + url);
+          }
+          break;
       }
     },
-    methods: {
-      full() {
-        if (!this.fullButton.full) {
-          this.fullScreen()
-          this.fullButton.full = true
-        } else {
-          this.fullExit()
-          this.fullButton.full = false
-        }
+    full() {
+      if (!this.fullButton.full) {
+        this.fullScreen();
+        this.fullButton.full = true;
+      } else {
+        this.fullExit();
+        this.fullButton.full = false;
       }
     },
     fullScreen() {
-      var element = document.documentElement
+      var element = document.documentElement;
       if (window.ActiveXObject) {
-          var WsShell = new ActiveXObject('WScript.Shell')
-          WsShell.SendKeys('{F11}')
+        var WsShell = new ActiveXObject("WScript.Shell");
+        WsShell.SendKeys("{F11}");
       } else if (element.requestFullScreen) {
-          element.requestFullScreen()
+        element.requestFullScreen();
       } else if (element.msRequestFullscreen) {
-          element.msRequestFullscreen()
+        element.msRequestFullscreen();
       } else if (element.webkitRequestFullScreen) {
-          element.webkitRequestFullScreen()
+        element.webkitRequestFullScreen();
       } else if (element.mozRequestFullScreen) {
-          element.mozRequestFullScreen()
+        element.mozRequestFullScreen();
       }
     },
     fullExit() {
-      var element = document.documentElement
+      var element = document.documentElement;
       if (window.ActiveXObject) {
-          var WsShell = new ActiveXObject('WScript.Shell')
-          WsShell.SendKeys('{F11}')
+        var WsShell = new ActiveXObject("WScript.Shell");
+        WsShell.SendKeys("{F11}");
       } else if (element.requestFullScreen) {
-          document.exitFullscreen()
+        document.exitFullscreen();
       } else if (element.msRequestFullscreen) {
-          document.msExitFullscreen()
+        document.msExitFullscreen();
       } else if (element.webkitRequestFullScreen) {
-          document.webkitCancelFullScreen()
+        document.webkitCancelFullScreen();
       } else if (element.mozRequestFullScreen) {
-          document.mozCancelFullScreen()
+        document.mozCancelFullScreen();
       }
     },
+    changeTime(time) {
+      this.$refs.music.currentTime = time
+    },
+    formatTime(time) {
+      let it = parseInt(time)
+      let m = parseInt(it / 60)
+      let s = parseInt(it % 60)
+
+      return (m < 10 ? "0" : "") + parseInt(it / 60) + ":" + (s < 10 ? "0" : "") + parseInt(it % 60)
+    },
+    changeVolume(v) {
+      this.music.volume += v
+      if (this.music.volume > 100) {
+        this.music.volume = 100
+      }
+      if (this.music.volume < 0) {
+        this.music.volume = 0
+      }
+      this.$refs.music.volume = this.music.volume / 100
+    },
+    play() {
+      if (this.$refs.music.paused) {
+        this.$refs.music.play()
+      } else {
+        this.$refs.music.pause()
+      }
+      this.music.isPlay = !this.$refs.music.paused
+      this.$nextTick(() => {
+        document.getElementById('play').blur()
+      })
+    }
   }
+};
 </script>
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
 .page-header {
-    padding: 5rem 6rem;
-    color: #fff;
-    text-align: center;
-    background-color: #159957;
-    background-image: linear-gradient(120deg, #155799, #159957);
+  padding: 5rem 6rem;
+  color: #fff;
+  text-align: center;
+  background-color: #159957;
+  background-image: linear-gradient(120deg, #155799, #159957);
 }
 
 .project-name {
-    font-size: 3.25rem;
-    margin-top: 0;
-    margin-bottom: 0.1rem;
+  font-size: 3.25rem;
+  margin-top: 0;
+  margin-bottom: 0.1rem;
 }
 
 .project-tagline {
-    font-size: 1.25rem;
-    margin-bottom: 2rem;
-    font-weight: normal;
-    opacity: 0.7;
+  font-size: 1.25rem;
+  margin-bottom: 2rem;
+  font-weight: normal;
+  opacity: 0.7;
 }
 
 .btn:hover {
-    color: rgba(255, 255, 255, 0.8);
-    text-decoration: none;
-    background-color: rgba(255, 255, 255, 0.2);
-    border-color: rgba(255, 255, 255, 0.3);
+  color: rgba(255, 255, 255, 0.8);
+  text-decoration: none;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-color: rgba(255, 255, 255, 0.3);
 }
 
 a:hover {
-    text-decoration: underline;
+  text-decoration: underline;
 }
 
 a:active,
 a:hover {
-    outline: 0;
+  outline: 0;
 }
 
 .btn {
-    padding: 0.75rem 1rem;
-    display: inline-block;
-    margin-bottom: 1rem;
-    color: rgba(255, 255, 255, 0.7);
-    background-color: rgba(255, 255, 255, 0.08);
-    border-color: rgba(255, 255, 255, 0.2);
-    border-style: solid;
-    border-width: 1px;
-    border-radius: 0.3rem;
-    transition: color 0.2s, background-color 0.2s, border-color 0.2s;
+  padding: 0.75rem 1rem;
+  display: inline-block;
+  margin-bottom: 1rem;
+  color: rgba(255, 255, 255, 0.7);
+  background-color: rgba(255, 255, 255, 0.08);
+  border-color: rgba(255, 255, 255, 0.2);
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 0.3rem;
+  transition: color 0.2s, background-color 0.2s, border-color 0.2s;
 }
 
 a {
-    color: #1e6bb8;
-    text-decoration: none;
+  color: #1e6bb8;
+  text-decoration: none;
 }
 
-.btn+.btn {
-    margin-left: 1rem;
+.btn + .btn {
+  margin-left: 1rem;
 }
 
 .main-content {
-    max-width: 64rem;
-    padding: 30px 0px 30px 0px;
-    margin: 0 auto;
-    font-size: 1.1rem;
-    word-wrap: break-word;
-    min-height: 800px;
+  max-width: 64rem;
+  padding: 30px 0px 30px 0px;
+  margin: 0 auto;
+  font-size: 1.1rem;
+  word-wrap: break-word;
+  min-height: 800px;
 }
 
 .foot {
-    max-width: 67rem;
-    margin: 0 auto;
-    font-size: 12px !important;
-    color: #586069 !important;
-    word-wrap: break-word;
+  max-width: 67rem;
+  margin: 0 auto;
+  font-size: 12px !important;
+  color: #586069 !important;
+  word-wrap: break-word;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Layout.vue'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.use(ElementUI)
 }
